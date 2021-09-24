9e40ee199d399f522f75f7af2f76e03fe741ab91
Peter <chenpeter0821@gmail.com>
2021-09-24 12:58:02 -0400

    Save

--- screens/Orders/OrderHomePage.js
+++ screens/Orders/OrderHomePage.js
@@ -1,64 +1,58 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  FlatList,
-  SectionList,
-} from "react-native";
-import { removeUser } from "../../redux";
-import { connect } from "react-redux";
-import Item from "../../components/Item";
-import { Row, ScrollView, Text } from "native-base";
-import { fontWeight, right, style } from "styled-system";
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, View, TouchableOpacity, FlatList } from 'react-native'
+import { removeUser } from '../../redux'
+import { connect } from 'react-redux'
+import Item from '../../components/Item'
+import { Text } from 'native-base'
+
 const OrderHomePage = (props) => {
-  const [count, setCount] = useState([]);
+  const [count, setCount] = useState([])
   const coffeeData = [
     {
-      name: "Bean 1",
-      size: "3oz",
+      name: 'Bean 1',
+      size: '3oz',
     },
     {
-      name: "Bean 2",
-      size: "5oz",
+      name: 'Bean 2',
+      size: '5oz',
     },
     {
-      name: "Bean 3",
-      size: "3oz",
+      name: 'Bean 3',
+      size: '3oz',
     },
     {
-      name: "Bean 4",
-      size: "3oz",
+      name: 'Bean 4',
+      size: '3oz',
     },
     {
-      name: "Bean 5",
-      size: "7oz",
+      name: 'Bean 5',
+      size: '7oz',
     },
     {
-      name: "Bean 6",
-      size: "9oz",
+      name: 'Bean 6',
+      size: '9oz',
     },
     {
-      name: "Bean 7",
-      size: "2oz",
+      name: 'Bean 7',
+      size: '2oz',
     },
     {
-      name: "Bean 8",
-      size: "8oz",
+      name: 'Bean 8',
+      size: '8oz',
     },
-  ];
+  ]
 
   const removeReduxUser = () => {
-    props.removeUserData();
-  };
-  const comfirmOrder = () => {};
+    props.removeUserData()
+  }
+  const comfirmOrder = () => {}
   const renderItem = ({ item, index }) => {
-    return <Item name={item.name} size={item.size} key={index}></Item>;
-  };
+    return <Item name={item.name} size={item.size} key={index}></Item>
+  }
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <FlatList
         data={coffeeData}
         numColumns={1}
@@ -67,16 +61,12 @@ const OrderHomePage = (props) => {
         ListHeaderComponent={() => {
           return (
             <View style={styles.titleContainer}>
-              <TouchableOpacity>
-                <Text style={styles.productDetails}>Product</Text>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text style={styles.productDetails}>Quanitity</Text>
-              </TouchableOpacity>
+              <Text style={styles.productDetails}>Product</Text>
+              <Text style={styles.productDetails}>Quanitity</Text>
             </View>
-          );
+          )
         }}
-      ></FlatList>
+      />
 
       <View style={styles.buttonView}>
         <TouchableOpacity style={styles.loginButton} onPress={removeReduxUser}>
@@ -86,51 +76,51 @@ const OrderHomePage = (props) => {
           <Text style={styles.loginButtonText}>Comfirm Order</Text>
         </TouchableOpacity>
       </View>
-    </ScrollView>
-  );
-};
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   titleContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: 20,
     paddingRight: 45,
     borderBottomWidth: 1,
-    borderBottomColor: "rgba(211,211,211,0.5)",
+    borderBottomColor: 'rgba(211,211,211,0.5)',
   },
   productDetails: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
 
   buttonView: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
   loginButton: {
-    justifyContent: "center",
+    justifyContent: 'center',
     paddingVertical: 12,
     paddingHorizontal: 32,
     borderRadius: 5,
-    backgroundColor: "black",
+    backgroundColor: 'black',
     opacity: 0.8,
     flex: 1 / 2,
     marginTop: 20,
   },
   loginButtonText: {
-    color: "white",
-    textAlign: "center",
+    color: 'white',
+    textAlign: 'center',
     fontSize: 12,
   },
-});
+})
 const mapDispatch = (dispatch) => {
   return {
     removeUserData: () => dispatch(removeUser()),
-  };
-};
-export default connect(null, mapDispatch)(OrderHomePage);
+  }
+}
+export default connect(null, mapDispatch)(OrderHomePage)
