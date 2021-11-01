244bc5669d22800d0768197ca271e4fc57cc3b17
LandingHeart <shinan.liu@yahoo.com>
2021-11-01 19:50:10 -0400

    logic problem not done multi page pdf

--- screens/Confirm/ConfirmationPage.js
+++ screens/Confirm/ConfirmationPage.js
@@ -33,7 +33,6 @@ const ConfirmationPage = (props) => {
   const [isPicker, setIsPicker] = useState(false)
   const rbsheetRef = useRef()
   const [orders, setOrders] = useState([])
-  const [pdfUrl, setPdfUrl] = useState()
   const [loading, setLoading] = useState(false)
   useEffect(() => {
     // map all the data from redux
@@ -53,7 +52,145 @@ const ConfirmationPage = (props) => {
     })
     setOrders(copyData)
   }, [])
+  const insertMultiPageHtml = () => {
+    var htmlArr = []
+    var tempStr = []
+    var itemStr = []
+    var pageWrapStr = ''
+    for (let order of orders) {
+      for (let item of order.data) {
+        tempStr.push(`<tr>
+        <td style="text-align:left">${item.name}</td>
+        <td>${item.count}</td>
+        <td>${0}</td>
+        <td>${0}</td>
+        </tr> `)
+      }
+    }
+    if (orders.length > 4) {
+      console.log('444444')
+      for (var j = 0; j < tempStr.length; j++) {
+        let counter = 0
+        if (counter < 4) {
+          var tempJ = tempJ + tempStr[j]
+          counter++
+        } else {
+          itemStr.push(tempJ)
+          counter = 0
+        }
+        tempJ = ''
+      }
+      for (var i = 0; i < itemStr.length; i++) {
+        pageWrapStr = insertHtml(
+          itemStr[i],
+          selectedFromValue,
+          selectedToValue,
+          '11-01-2021',
+          '#1234567890'
+        )
+        htmlArr.push(pageWrapStr)
+      }
+    } else {
+      tempStr.join('')
+      pageWrapStr = insertHtml(
+        tempStr,
+        selectedFromValue,
+        selectedToValue,
+        '11-01-2021',
+        '#1234567890'
+      )
+      htmlArr.push(pageWrapStr)
+    }
+    htmlArr.join('')
 
+    const htmlContent = `
+    <!DOCTYPE html>
+      <html>
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Oswald:wght@200&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300&family=Oswald:wght@200&display=swap"
+        rel="stylesheet"
+      />
+          </head>
+          <style>
+            * {
+              margin: 0;
+              padding: 0;
+            }
+            body {
+              font: 14px/1.4 Georgia, serif;
+            }
+            #page-wrap {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              height: 800px;
+              width: 540px;
+              margin: 0 auto;
+              
+            }
+            table {
+              border-collapse: collapse;
+            }
+            td {
+              padding: 15px;
+              font-size: 12px;
+            }
+            th {
+              padding-bottom: 5px;
+              padding-top: 5px;
+            }
+            tr:nth-child(even) {
+              background-color: #dddddd;
+            }
+        
+            .title-tr {
+              background-color: black;
+              color: white;
+            }
+            .row:after {
+              content: '';
+              display: table;
+              clear: both;
+            }
+            .col {
+              float: left;
+              width: 50%;
+            }
+            .col-2 {
+              float: right;
+              width: 50%;
+            }
+            td {
+              text-align: center;
+              font-size: 12px;
+            }
+            h1 {
+              font-family: 'Oswald';
+              font-weight: 900;
+              font-size: xx-large;
+            }
+            #subheading {
+              font-family: 'Montserrat', sans-serif;
+              font-size: 12px;
+            }
+          </style>
+          <body>
+            ${htmlArr}
+          </body>
+        </html>
+    `
+    return htmlContent
+  }
   const openPickerStore = () => {
     setIsPicker(true)
     rbsheetRef.current.open()
@@ -75,15 +212,13 @@ const ConfirmationPage = (props) => {
         url,
         FileSystem.documentDirectory + 'invoice-20.pdf'
       )
-      await Sharing.shareAsync(uri)
-      await AsyncStorage.clear()
+      props.navigation.navigate('PdfView', {
+        uri,
+      })
       console.log('Finished downloading to ', uri)
     } catch (error) {
       console.error(error)
     }
-    props.navigation.navigate('PdfView', {
-      uri,
-    })
   }
 
   const createOrderString = () => {
@@ -125,7 +260,8 @@ const ConfirmationPage = (props) => {
     const orderString = createOrderString()
     setLoading(true)
     if (orders.length !== 0) {
-      const html = insertHtml(orders)
+      const html = insertMultiPageHtml()
+      console.log('html: ', html)
       axios
         .post(
           'http://b61a-216-158-137-35.ngrok.io/api/fillupSupplyAPI/sendSms',
@@ -162,7 +298,7 @@ const ConfirmationPage = (props) => {
     props.resetOrder()
     setOrders([])
     setTimeout(() => {
-      Alert.alert('Ordered Success', 'press ok to save a copy', [
+      Alert.alert('Ordered Success', 'press ok to view invoice', [
         {
           text: 'Ok',
           onPress: () => sharePdf(url),
@@ -173,7 +309,7 @@ const ConfirmationPage = (props) => {
           style: 'cancel',
         },
       ])
-    }, 2000)
+    }, 200)
   }
   const renderItem = ({ item }) => {
     if (item.count > 0) {
--- screens/PdfViewer/PdfView.js
+++ screens/PdfViewer/PdfView.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
+import { Button } from 'native-base'
 import PDFReader from 'rn-pdf-reader-js'
+import * as Sharing from 'expo-sharing'
 
 export default function PdfView(props) {
+  const { uri } = props.route.params
   return (
-    <PDFReader
-      source={{
-        uri: props.uri,
-      }}
-    />
+    <View style={styles.container}>
+      <PDFReader
+        source={{
+          uri,
+        }}
+      />
+      <Button
+        onPress={() => Sharing.shareAsync(uri)}
+        style={{ backgroundColor: 'black', borderRadius: 0 }}
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}>Send a copy</Text>
+      </Button>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
