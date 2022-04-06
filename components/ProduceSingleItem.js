a778269c8b5909b126a88102d3d3f4c438b3fcdb
Peter <chenpeter0821@gmail.com>
2022-04-06 17:15:29 -0400

    save

--- components/ProduceSingleItem.js
+++ components/ProduceSingleItem.js
@@ -1,24 +1,98 @@
 import React, { memo } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import { Box } from 'native-base'
 import AppIcons from './AppIcons'
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { Badge } from 'react-native-paper'
 
 const Item = memo(
   ({ order, removeItem, addItem }) => {
+    console.log(order)
     return (
-      <Box style={styles.box}>
-        <View
-          style={{
-            flex: 1.5,
-            flexWrap: 'wrap',
-            flexDirection: 'row',
-            paddingRight: 15,
-          }}
-        >
-          <Text style={{ color: 'white', fontSize: 25 }}>{order.name}</Text>
-        </View>
-        <View style={styles.actionBox}>
+      <TouchableWithoutFeedback onPress={addItem}>
+        <Box style={styles.box}>
+          <View
+            style={{
+              flex: 1.5,
+              // flexWrap: 'wrap',
+              flexDirection: 'row',
+              paddingRight: 15,
+              justifyContent: 'space-between',
+              // alignContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            {order.count !== 0 && (
+              <Badge
+                size={45}
+                style={{
+                  position: 'absolute',
+                  top: -10,
+                  left: -10,
+                  fontWeight: 'bold',
+                  fontSize: 25,
+                  zIndex: 99,
+                }}
+              >
+                {order.count}
+              </Badge>
+            )}
+            <Image
+              source={{
+                uri: order.url
+                  ? order.url
+                  : 'https://fillupstore.s3.amazonaws.com/default_coffee_2.jpg',
+              }}
+              style={{ height: 100, width: 100, borderRadius: 5 }}
+            />
+            <Text style={{ color: 'white', fontSize: 25, fontWeight: 'bold' }}>
+              {order.name}
+            </Text>
+          </View>
+        </Box>
+      </TouchableWithoutFeedback>
+    )
+  },
+  (prev, next) => {
+    const prevCount = prev.order.count
+    const nextCount = next.order.count
+    const isEqual = prevCount === nextCount
+    return isEqual
+  }
+)
+
+const styles = StyleSheet.create({
+  box: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(211,211,211,0.5)',
+    paddingLeft: 15,
+    width: '100%',
+    height: 150,
+  },
+  actionBox: {
+    marginRight: 10,
+    width: '40%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  orderTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  orderSize: {
+    color: 'white',
+  },
+})
+
+export default Item
+
+{
+  /* <View style={styles.actionBox}>
           <TouchableWithoutFeedback
             onPress={removeItem}
             style={{
@@ -68,44 +142,5 @@ const Item = memo(
               style={{ alignSelf: 'center' }}
             />
           </TouchableWithoutFeedback>
-        </View>
-      </Box>
-    )
-  },
-  (prev, next) => {
-    const prevCount = prev.order.count
-    const nextCount = next.order.count
-    const isEqual = prevCount === nextCount
-    return isEqual
-  }
-)
-
-const styles = StyleSheet.create({
-  box: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgba(211,211,211,0.5)',
-    paddingLeft: 15,
-    width: '100%',
-    height: 90,
-  },
-  actionBox: {
-    marginRight: 10,
-    width: '40%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  orderTitle: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  orderSize: {
-    color: 'white',
-  },
-})
-
-export default Item
+        </View> */
+}
